5f8afe08f2314f4b17d4100abc2fd14dd4e1faa1
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-20 18:47:18 -0300

    :construction: cli: Adicionando schedule

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Console\Commands\TweetDollarCotation;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -15,4 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command(TweetDollarCotation::class)->cron('0 8-17 * * 1-5');
     })->create();
