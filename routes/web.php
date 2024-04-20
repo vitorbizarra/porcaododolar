3d3deb0938f210cfd05023818822565cfaceb2be
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-20 18:18:50 -0300

    :fire: Removendo rotas não necessárias

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,7 @@
 <?php
 
-use Atymic\Twitter\Contract\Http\Client;
-use Atymic\Twitter\Facade\Twitter;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/tweet', function () {
-    $querier = Twitter::forApiV2()->getQuerier();
-    $result = $querier->post(
-        'tweets',
-        [
-            Client::KEY_REQUEST_FORMAT => Client::REQUEST_FORMAT_JSON,
-            'text' => "my tweet com quebra de linha"
-        ]
-    );
-
-    dd($result);
 });
