b709b51ba863f191814c3cf8536b227fcd670dd5
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-20 17:00:56 -0300

    :construction: wip: Função enviar tweet

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Atymic\Twitter\Contract\Http\Client;
+use Atymic\Twitter\Facade\Twitter;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/tweet', function () {
+    $querier = Twitter::forApiV2()->getQuerier();
+    $result = $querier->post(
+        'tweets',
+        [
+            Client::KEY_REQUEST_FORMAT => Client::REQUEST_FORMAT_JSON,
+            'text' => "my tweet com quebra de linha"
+        ]
+    );
+
+    dd($result);
 });
